8171a61dba80094c01945d1be8d3bde3755ad155
Darshan1209 <123211130+Darshan1209@users.noreply.github.com>
2024-10-11 20:05:45 +0300

    seo attempt 2

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,7 +3,6 @@ export const contactsData = {
     phone: '+254734528858',
     address: 'BAPS Temple, Nairobi, Kenya',
     github: 'https://github.com/Darshan1209',
-    facebook: '',
     linkedIn: 'https://www.linkedin.com/in/darshan-patel-027b55251/',
     twitter: 'https://x.com/Dots_1209',
     stackOverflow: 'https://stackoverflow.com/users/27205496/darshan-patel',
